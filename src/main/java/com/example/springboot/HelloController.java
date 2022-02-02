04f66f1ec18d56f367c6e0d83234dfce9668219b
Mae Anne Large <Mpluya@users.noreply.github.com>
2022-02-02 13:07:46 -0700

    nudge

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu + day2.5!";
+		return "Greetings from Spring Boot + Tanzu + day3!";
 	}
 
 }
