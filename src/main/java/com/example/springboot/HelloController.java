31525e89799d4d2cd7cc65448a431bb97ac48b41
Mae Anne Large <Mpluya@users.noreply.github.com>
2022-02-02 13:27:43 -0700

    rate limiting is fun

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu + day3!";
+		return "Greetings from Spring Boot + Tanzu + day-3!";
 	}
 
 }
